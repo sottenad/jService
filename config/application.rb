4c8765fa4b690c98f78bd9de28e817f393183054
Steve Ottenad <sottenad@gmail.com>
2014-02-09 22:30:47 -0800

    turning init_on_precompile to false to satisfy heroku

--- config/application.rb
+++ config/application.rb
@@ -55,6 +55,9 @@ module JArchive
 
     # Enable the asset pipeline
     config.assets.enabled = true
+    
+    # Disable this to get it to not precompile on heroku when it wont succeed
+    config.assets.initialize_on_precompile = false
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
