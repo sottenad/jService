2f7c85fd3312d8751137a1a6aa1d7f5944c0119c
Steve Ottenad <sottenad@gmail.com>
2014-02-10 21:08:51 -0800

    precompile

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -16,7 +16,6 @@ JArchive::Application.configure do
 
   # Don't fallback to assets pipeline if a precompiled asset is missed
   config.assets.compile = true
-  
 
   # Generate digests for assets URLs
   config.assets.digest = true
