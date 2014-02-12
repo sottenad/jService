1e4fa1ad6a05ef4f75e03832fd03fb685e09f54f
Steve Ottenad <sottenad@gmail.com>
2014-02-12 12:41:27 -0800

    precompiling

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,9 +15,3 @@
 //= require_tree .
 
 
-Handlebars.registerHelper('json', function(context) {
-    return JSON.stringify(context);
-});
-Handlebars.registerHelper('clean_time', function(context) {
-    return moment(context).format("MMMM Do, YYYY");
-});
