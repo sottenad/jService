eb4ea4d32870c47cd92b1a727b3a19d6d4d2d421
Steve Ottenad <sottenad@gmail.com>
2023-07-05 19:44:22 -0700

    removing blacklist

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -4,8 +4,8 @@ Rack::Attack.throttle("requests by ip", limit: 10, period: 30) do |request|
   
 
 
-bad_ips = ['34.70.41.112', '23.159.16.216'] # Pulled from a external API on app boot.
-# Block these IPs
-Rack::Attack.blocklist "block known bad IP address" do |req|
-  bad_ips.include?(req.ip)
-end
+# bad_ips = ['34.70.41.112', '23.159.16.216'] # Pulled from a external API on app boot.
+# # Block these IPs
+# Rack::Attack.blocklist "block known bad IP address" do |req|
+#   bad_ips.include?(req.ip)
+# end
