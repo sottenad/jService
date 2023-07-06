c2df75cf60348500394ebed18cdb74d8a63f05d0
Steve Ottenad <sottenad@gmail.com>
2023-07-05 18:37:12 -0700

    blocking IP's

--- config/initializers/rack_attack.rb
+++ config/initializers/rack_attack.rb
@@ -1,4 +1,9 @@
-Rack::Attack.throttle("requests by ip", limit: 30, period: 60) do |request|
+Rack::Attack.throttle("requests by ip", limit: 10, period: 60) do |request|
     request.ip
   end
   
+# Block requests from 1.2.3.4
+Rack::Attack.blacklist('block 34.70.41.112') do |req|
+  # Requests are blocked if the return value is truthy
+  '34.70.41.112' == req.ip
+end
